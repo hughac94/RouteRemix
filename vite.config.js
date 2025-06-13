2785b8d7a0522d6b040a1a4b79fe678fbb1cc792
Hugh Chatfield <hugh.chatfield@generation.org>
2025-06-13 09:01:15 +0100

    save

--- vite.config.js
+++ vite.config.js
@@ -1,13 +1,13 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import path from 'path'
+import { fileURLToPath, URL } from 'node:url'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/RouteRemix/', // Replace with your repository name
+  base: '/RouteRemix/',
   resolve: {
     alias: {
-      "@": path.resolve(__dirname, "./src"),
+      "@": fileURLToPath(new URL('./src', import.meta.url))
     },
   },
 })
