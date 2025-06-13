5f8df624fffb57f5ef3f1a2400b78c96b330a33f
Hugh Chatfield <hugh.chatfield@generation.org>
2025-06-13 09:03:38 +0100

    rename

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import MapView from "./Components/Mapview";
+import MapView from "./components/Mapview";
 import { Card, CardContent, CardDescription, CardHeader, CardTitle } from "@/components/ui/card";
 import { Badge } from "@/components/ui/badge";
 import { Button } from "@/components/ui/button";
